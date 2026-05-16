c9b2ddc812ee7471b761a98e0a09bbcfd7fc0f41
yevpy <eugene2piskun@gmail.com>
2026-05-17 00:25:03 +0300

    add project config

---
+++ config.py
@@ -0,0 +1,14 @@
+BASE_URL = "https://jobs.dou.ua/vacancies/"
+DATE_FORMAT = "%Y-%m-%d"
+
+EXPERIENCE_LEVEL = {
+    "junior/trainee": "0-1",
+    "middle": "1-3",
+    "senior": "3-5",
+    "senior+": "5plus"
+}
+
+CATEGORY = "Python"
+
+RAW_DATA_DIR = "data/raw/"
+CHARTS_DIR = "data/charts/"
